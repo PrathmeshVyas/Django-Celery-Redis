871d3e5bb7b537e12a7aeec11c7baf5134de6b32
PrathmeshVyas <prathmeshvyas2002@gmail.com>
2024-08-12 01:54:49 +0530

    Added taks rate limits

--- command.md
+++ command.md
@@ -2,6 +2,8 @@ pip freeze > requirements.txt
 docker-compose up -d --build
 docker exec -it django /bin/sh
 
+tp1.delay()
+
 from celery import group
 from cworker.tasks import tp1, tp2, tp3, tp4
 task_group = group(tp1.s(), tp2.s(), tp3.s(), tp4.s())
--- django_celery/django_celery/celery.py
+++ django_celery/django_celery/celery.py
@@ -6,6 +6,8 @@ app = Celery("django_celery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.conf.task_routes = {'newapp.tasks.task1': {'queue':'queue1'}, 'newapp.tasks.task2': {'queue':'queue2'}}
 
+app.conf.task_default_rate_limit = '1/m'
+
 app.conf.broker_transport_options = {
     'priority_steps': list(range(10)),
     'sep':':',
