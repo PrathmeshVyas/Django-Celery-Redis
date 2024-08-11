d5477e20d271632b983888b0d57bb2d3f83e966f
PrathmeshVyas <prathmeshvyas2002@gmail.com>
2024-08-12 01:31:46 +0530

    Added celery queue, group, chaining

--- command.md
+++ command.md
@@ -2,3 +2,12 @@ pip freeze > requirements.txt
 docker-compose up -d --build
 docker exec -it django /bin/sh
 
+from celery import group
+from cworker.tasks import tp1, tp2, tp3, tp4
+task_group = group(tp1.s(), tp2.s(), tp3.s(), tp4.s())
+task_group.apply_async()
+
+from celery import chain
+from cworker.tasks import tp1, tp2, tp3, tp4
+task_chain = chain(tp1.s(), tp2.s(), tp3.s())
+task_chain.apply_async()
--- django_celery/django_celery/celery.py
+++ django_celery/django_celery/celery.py
@@ -4,9 +4,12 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery.settings')
 app = Celery("django_celery")  
 app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.task_routes = {'newapp.tasks.task1': {'queue':'queue1'}, 'newapp.tasks.task2': {'queue':'queue2'}}
 
-@app.task
-def add_numbers():
-    return
+app.conf.broker_transport_options = {
+    'priority_steps': list(range(10)),
+    'sep':':',
+    'queue_order_stratergy':'priority',
+}
 
 app.autodiscover_tasks()
